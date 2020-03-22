98e68eb2729c13cd2819480b53455b7d51f1dd27
Yichen <33232314+yidu2008@users.noreply.github.com>
2020-03-22 18:23:44 -0400

    Mar 21 18:21
    Added logic. Now program can detect if the password includes only passwords or not. also added array to combine alert messages.

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,18 +1,19 @@
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { IonicModule } from "@ionic/angular";
+import { RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { Tab2Page } from "./tab2.page";
+import { ExploreContainerComponentModule } from "../explore-container/explore-container.module";
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     ExploreContainerComponentModule,
-    RouterModule.forChild([{ path: '', component: Tab2Page }])
+    RouterModule.forChild([{ path: "", component: Tab2Page }])
   ],
   declarations: [Tab2Page]
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { ToastController } from "@ionic/angular";
 
 @Component({
   selector: "app-tab2",
@@ -6,5 +8,47 @@ import { Component } from "@angular/core";
   styleUrls: ["tab2.page.scss"]
 })
 export class Tab2Page {
-  constructor() {}
+  public password = new FormControl("");
+  public meterStrengthColor = "darkgreen";
+
+  constructor(public toastController: ToastController) {}
+
+  public async onRunProgram() {
+    const alertMessages: string[] = [];
+    console.log(this.password.value);
+
+    console.log(this.password.value.length);
+
+    if (this.password.value.length < 7) {
+      console.log("Your password is very short!");
+      alertMessages.push("Your password is very short!");
+    }
+    if (this.password.value.length > 6 && this.password.value.length < 12) {
+      console.log("Your password is short!");
+      alertMessages.push("Your password is  short!");
+    }
+    if (this.password.value.length > 11) {
+      console.log("Your password is long enough. Now, will you remember it?");
+      alertMessages.push(
+        "Your password is long enough. Now, will you remember it?"
+      );
+    }
+    if (this.password.value.length < 1) {
+      console.log("You didn't enter a password!");
+      alertMessages.push("You didn't enter a password!");
+    }
+    if (/^[a-zA-Z]+$/gi.test(this.password.value)) {
+      console.log("Your password contains only letters. This isn't good!");
+      alertMessages.push(
+        "Your password contains only letters. This isn't good!"
+      );
+    }
+    if (alertMessages.length > 0) {
+      const toast = await this.toastController.create({
+        message: alertMessages.join("\n"),
+        duration: 5000
+      });
+      toast.present();
+    }
+  }
 }
